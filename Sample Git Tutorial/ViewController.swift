77fde7fa1cc994300cc40ff390b456da68872fa6
Hemaraju MacMini <hemaraju@incipio.com>
2018-12-11 17:27:48 +0530

    Initial Commit

---
+++ Sample Git Tutorial/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Sample Git Tutorial
+//
+//  Created by Hemaraju MacMini on 11/12/18.
+//  Copyright © 2018 UHemarajU. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        var welcome: String
+        welcome = "Hello World"
+        print(welcome)
+     
+        
+    }
+
+  
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
